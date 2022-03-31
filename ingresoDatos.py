9f32f738fc0f384b89fdd883c38d0bd41a38e183
DevPanch02 <edison.molina6366@utc.edu.ec>
2022-03-31 13:04:20 -0500

    Ingreso de datos

---
+++ ingresoDatos.py
@@ -0,0 +1,13 @@
+#INGRESO DE DATOS
+valor=int(input("Ingresa valor entero: "))
+valor1=float(input("Ingresa valor decimal: "))
+
+
+#valor2=str(input("Ingrese cadena de texto: "))
+#o
+valor2=input("Ingrese cadena: ")
+
+
+
+print("--------------------------------------")
+print(f"Valor entero: {valor}\nValor decimal: {valor1}\nValor str: {valor2} ")
