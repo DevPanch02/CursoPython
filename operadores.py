504115c0de818101a309efdd702f4297f227e11b
DevPanch02 <edison.molina6366@utc.edu.ec>
2022-03-31 13:03:33 -0500

    Operadores

---
+++ operadores.py
@@ -0,0 +1,42 @@
+#OPERADORES ARITMETICOS
+#SON UTILIZADOS PARA REALIZAR OPERACIONES
+#MANEJANDO DATOS
+
+# a=10
+# b=2
+# suma = a+b
+
+# print("Suma: {} ".format(suma))  #<--> Suma o adicion '+'
+# print(f"Resta: {a-b} ")  #<--> Resta o sustraccion '-'
+# print("Multiplicacion: {} ".format(a*b))  #<--> multiplicacion '*'
+# print("Division: {} ".format(a/b))  #<--> Division '/'
+# print("Division truncada: {} ".format(a//b)) #<--> Division truncada  '//'
+# print("Modulo division: {} ".format(a%b))  #<--> Modulo de division  '%'
+# print("Exponenciacion: {} ".format(a**b)) #Exponenciacion  '**'
+
+# #TIPO STR
+# a1='hola '
+# b1='Dev'
+
+# print("Mensaje: {} ".format(a1+b1))
+
+#OPERADORES LOGICOS
+#and(y) or(o) not(no)
+
+#   AND
+print("-------AND----------")
+print(True and True) # 1 y 1 =1
+print(True and False)
+print(False and False)
+
+#   OR
+print("-------OR----------")
+print(True or True)
+print(True or False)
+print(False or False)
+
+#   NOT
+print("-------NOT----------")
+print(not True)
+print(not False)
+
