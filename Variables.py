da67b426715313411d5d41da5551bb2490f4e191
DevPanch02 <edison.molina6366@utc.edu.ec>
2022-03-31 13:02:37 -0500

    Variables

---
+++ Variables.py
@@ -0,0 +1,30 @@
+#VARIABLES 
+
+#ASIGNACION DE VALORES
+
+#RESTRICCIONES
+#   NO LLEVAR ESPACIOS -> variable uno
+#   NO INICIAR CON NUMEROS -> 10valor
+#   NO PALABRAS RESERVADAS -> if / for / while
+
+
+#TIPOS DE DATOS
+
+#ENTERO  ->    int
+#FLOTANTES  ->  float
+#CADENAS  ->   str
+#VERDADERO Y FALSO   ->  bool
+
+#TIPO DE DATO ENTERO   -> int = 1,2,3,4,....,n
+#TIPO DE DATO FLOTANTE  -> float = 1.2,2.1,...,n.n
+#TIPO DE DATO CADENA o STRING   -> str ='hola mundo'
+#TIPO DE DATO VERDADERO o FALSO   -> bool=True
+
+valor1=10
+valor2=10.1
+valor3='hola mundo'
+valor4=True
+
+print("El primer valor es: ",valor1)
+print(f"El segundo es: {valor2} ",type(valor2))
+print("El tercero valor es: {} ".format(valor3),type(valor3))
