b0a0195009cf43373dafe917fde86062479dbad2
DevPanch02 <edison.molina6366@utc.edu.ec>
2022-03-31 22:57:06 -0500

    Indices y slicings

---
+++ indices.py
@@ -0,0 +1,11 @@
+#   INDICES
+texto = 'Hola-Dev',1
+print(texto[0],texto[1],texto[2],texto[3])
+
+#   SLICING (rebanar o separar)
+#-8 -7  -6  -5  -4  -3  -2  -1
+#H  O   L   A   -   D   E   V
+#0  1   2   3   4   5   6   7
+
+print(texto[:-1])
+
