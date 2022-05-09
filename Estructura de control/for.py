a705378502f1c548e421c9322100efd2fe6f386e
DevPanch02 <edison.molina6366@utc.edu.ec>
2022-05-09 07:24:24 -0500

    Ciclo for

---
+++ Estructura de control/for.py
@@ -0,0 +1,48 @@
+#   BUCLE SIMILAR A WHILE,
+#   SIRVE PARA IMPRIMIR VARIOS VALORES
+#   YA SEA DE UNA LISTA ENTRE OTRAS
+
+# lista=[1,2,3,4,8,6,7]
+# indice=0
+
+
+# while (indice < len(lista)):
+#     print(lista[indice])
+#     indice = indice + 1
+
+#   CADENA DE TEXTO
+# cadena="hola dev"
+# for cad in cadena:
+#     print(cad)
+#//////////////////////
+#   RANGO
+# valor=int(input("Ingrese valor: "))
+# for i in range(valor):
+#     multiplacion=i*valor
+#     print(f" {i} * {valor} = {multiplacion} ")
+
+#//////////////////////
+
+#   INGRESO DATOS EN LISTAS
+
+# nombres=[]
+# cedulas=[]
+
+# elementos=int(input("Ingrese cantidad de personas: "))
+
+# for i in range(elementos):
+#     print(f"Ingrese datos de la persona {i+1} ")
+#     nombre=input("Ingrese nombre: ")
+#     cedula=int(input("Ingrese cedula: "))
+    
+#     nombres.append(nombre)
+#     cedulas.append(cedula)
+
+
+# for i in range(elementos):
+#     print("------------------------")
+#     print(f"PERSONA {i+1} ")
+#     print(f"NOMBRE: {nombres[i]} ")
+#     print(f"CEDULA: {cedulas[i]} ")
+
+
