aaedd51f870c723150c5d7d3a9263527043f1eeb
DevPanch02 <edison.molina6366@utc.edu.ec>
2022-05-16 08:17:48 -0500

    While

--- Estructura de control/bucles.py
+++ Estructura de control/bucles.py
@@ -26,13 +26,30 @@
 
 #   CONTINUE
 #   IDENTIFICAR VALOR Y SEGUIR
-print("-----------------------------------------------------------------")
-contador=0
-while contador<=10:
-    contador=contador+1
-    if contador == 6:
-        print(f"Se identifica valor y se sigue: {contador} ")
-        continue
-    print(f"Valor: {contador} ")
-else:
-    print(f"Las iteraciones: {contador} ")
+# print("-----------------------------------------------------------------")
+# contador=0
+# while contador<=10:
+#     contador=contador+1
+#     if contador == 6:
+#         print(f"Se identifica valor y se sigue: {contador} ")
+#         continue
+#     print(f"Valor: {contador} ")
+# else:
+#     print(f"Las iteraciones: {contador} ")
+
+# while True:
+#     numeros=int(input("Ingrese valor: "))
+#     if numeros % 2 == 0:
+#         print(f"El numero {numeros}, es par ")
+#         break
+#     else:
+#         print("Ingrese nuevamente..!!")
+
+lista=[1,2,3,4,8,6,7]
+indice=0
+print(len(lista))
+print("----------------")
+
+while (indice < len(lista)):
+    print(lista[indice])
+    indice+=1
