adb9087ba827794869e2012dd9cae3696e424228
DevPanch02 <edison.molina6366@utc.edu.ec>
2022-06-10 09:23:46 -0500

    Clases y objetos

---
+++ POO/poo.py
@@ -0,0 +1,15 @@
+#   PROGRAMACION ORIENTADA A OBJETOS
+#   CLASES Y OBJETOS
+
+class Perro:
+    pass
+
+print(Perro)
+
+# Perro perro = new Perro()
+
+perro = Perro()
+perro1 = Perro()
+
+print(perro)
+print(perro1)
